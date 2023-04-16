7dbe0dfd259f0af1f70da7741f282d1f5bada710
sol <dataenggu@gmail.com>
2023-04-16 21:14:14 +0200

    Add more email and password validation logic with added locale

--- lib/src/components/cust_text_field.dart
+++ lib/src/components/cust_text_field.dart
@@ -3,28 +3,27 @@ import 'package:flutter/material.dart';
 class CustTextField extends StatefulWidget {
   final TextEditingController textEditingController;
   final ValueChanged<String>? onChange;
+  final FocusNode? focusNode;
   final String? hintText;
   final bool? isInputInvalid;
   final String? errorText;
   final bool? isObscure;
   final int? maxLength;
 
-  const CustTextField(
-      {Key? key,
-      required this.textEditingController,
-      this.hintText,
-      this.isInputInvalid,
-      this.errorText,
-      this.isObscure,
-      this.maxLength,
-      this.onChange})
-      : super(key: key);
+  const CustTextField({
+    Key? key,
+    required this.textEditingController,
+    this.onChange,
+    this.focusNode,
+    this.hintText,
+    this.isInputInvalid,
+    this.errorText,
+    this.isObscure,
+    this.maxLength,
+  }) : super(key: key);
 
   String? _getErrorText() {
-    if (isInputInvalid != null &&
-        isInputInvalid! &&
-        errorText != null &&
-        textEditingController.text.isNotEmpty) {
+    if (isInputInvalid != null && isInputInvalid! && errorText != null) {
       return errorText!;
     }
     return null;
@@ -41,10 +40,12 @@ class _CustTextFieldState extends State<CustTextField> {
       cursorColor: Colors.black,
       maxLength: widget.maxLength,
       controller: widget.textEditingController,
+      focusNode: widget.focusNode,
       obscureText: widget.isObscure == null ? false : widget.isObscure!,
       decoration: InputDecoration(
           hintText: widget.hintText ?? "",
           errorText: widget._getErrorText(),
+          errorStyle: const TextStyle(fontSize: 15),
           border: const OutlineInputBorder(
               borderSide: BorderSide(color: Colors.black)),
           focusedBorder: const OutlineInputBorder(
--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -17,13 +17,34 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
   final commonService = CommonService();
 
+  late final emailFocusNode = FocusNode();
+  late final passwordFocusNode = FocusNode();
+
   bool _isEmailInvalid = false;
+  bool _isPasswordEmpty = false;
+
+  @override
+  void initState() {
+    super.initState();
+    emailFocusNode.addListener(() {
+      if (!emailFocusNode.hasFocus) {
+        _verifyEmail();
+      }
+    });
+    passwordFocusNode.addListener(() {
+      if (!passwordFocusNode.hasFocus) {
+        _verifyPasswordEmptiness();
+      }
+    });
+  }
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
   }
 
   void _onChangeEmail(String input) {
@@ -46,15 +67,44 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  void _onChangePassword(String input) {
+    if (input.isNotEmpty) {
+      setState(() {
+        _isPasswordEmpty = false;
+      });
+    }
+  }
+
+  void _verifyPasswordEmptiness() {
+    setState(() {
+      _isPasswordEmpty = _passwordController.text.isEmpty;
+    });
+  }
+
   void _onPressRegister() {
-    Fluttertoast.showToast(msg: "가입 gogo!!");
+    // TO-DO : register
+    Fluttertoast.showToast(msg: "TO-DO : register");
   }
 
   void _onPressLogin() {
-    if (_isEmailInvalid) {
-      Fluttertoast.showToast(msg: "입력값을 확인해주세요.");
+    _verifyEmail();
+    _verifyPasswordEmptiness();
+    emailFocusNode.unfocus();
+    passwordFocusNode.unfocus();
+    if (_isEmailInvalid || _emailController.text.isEmpty) {
+      emailFocusNode.requestFocus();
       return;
     }
+    if (_isPasswordEmpty || _passwordController.text.isEmpty) {
+      passwordFocusNode.requestFocus();
+      return;
+    }
+    // TO-DO : login
+  }
+
+  void _onTapOnScreenGesture() {
+    emailFocusNode.unfocus();
+    passwordFocusNode.unfocus();
   }
 
   @override
@@ -62,78 +112,81 @@ class _LoginPageState extends State<LoginPage> {
     final String emailHintText = "login".tr(gender: "email");
     final String passwordHintText = "login".tr(gender: "password");
 
-    return MaterialApp(
-        home: Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: CommonAppBar(
-          title: "login".tr(gender: "login_title"), showLeading: false),
-      body: Container(
-          color: Colors.blue,
-          child: Container(
-            margin: const EdgeInsets.only(top: 50),
-            child: Column(children: [
-              Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  child: Text("login".tr(gender: "login_content"),
-                      style: const TextStyle(fontSize: 25),
-                      textAlign: TextAlign.center)),
-              Container(
-                  margin: const EdgeInsets.all(15),
-                  child: Focus(
-                      onFocusChange: (value) => {if (!value) _verifyEmail()},
-                      child: CustTextField(
-                        onChange: _onChangeEmail,
-                        textEditingController: _emailController,
-                        isInputInvalid: _isEmailInvalid,
-                        errorText: "validation".tr(gender: "invalid_email"),
-                        hintText: emailHintText,
-                      ))),
-              Container(
-                margin: const EdgeInsets.all(15),
-                child: Focus(
+    return GestureDetector(
+      onTap: _onTapOnScreenGesture,
+      child: MaterialApp(
+          home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: CommonAppBar(
+            title: "login".tr(gender: "login_title"), showLeading: false),
+        body: Container(
+            color: Colors.blue,
+            child: Container(
+              margin: const EdgeInsets.only(top: 50),
+              child: Column(children: [
+                Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Text("login".tr(gender: "login_content"),
+                        style: const TextStyle(fontSize: 25),
+                        textAlign: TextAlign.center)),
+                Container(
+                    margin: const EdgeInsets.all(15),
                     child: CustTextField(
-                  textEditingController: _passwordController,
-                  maxLength: 15,
-                  hintText: passwordHintText,
-                  isObscure: true,
-                )),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: SizedBox(
-                    width: double.maxFinite,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
-                      onPressed: () {},
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(fontSize: 25),
-                      ),
+                      onChange: _onChangeEmail,
+                      focusNode: emailFocusNode,
+                      textEditingController: _emailController,
+                      isInputInvalid: _isEmailInvalid,
+                      errorText: "validation".tr(gender: "invalid_email"),
+                      hintText: emailHintText,
                     )),
-              )
-            ]),
-          )),
-      bottomSheet: Container(
-          color: Colors.blue,
-          child: Container(
-              margin: const EdgeInsets.all(15),
-              child:
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Text("login".tr(gender: "register_content"),
-                    style: const TextStyle(fontSize: 17),
-                    textAlign: TextAlign.center),
-                const SizedBox(width: 10),
-                TextButton(
-                    onPressed: _onPressRegister,
-                    style: TextButton.styleFrom(
-                        textStyle: const TextStyle(fontSize: 17),
-                        foregroundColor: Colors.yellow),
-                    child: Text(
-                      "login".tr(gender: "create_account"),
-                    ))
-              ]))),
-    ));
+                Container(
+                  margin: const EdgeInsets.all(15),
+                  child: CustTextField(
+                    onChange: _onChangePassword,
+                    focusNode: passwordFocusNode,
+                    textEditingController: _passwordController,
+                    isInputInvalid: _isPasswordEmpty,
+                    errorText: "validation".tr(gender: "empty_password"),
+                    hintText: passwordHintText,
+                    isObscure: true,
+                    maxLength: 15,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: SizedBox(
+                      width: double.maxFinite,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black),
+                        onPressed: _onPressLogin,
+                        child: Text("login".tr(gender: "login_btn"),
+                            style: const TextStyle(fontSize: 25)),
+                      )),
+                )
+              ]),
+            )),
+        bottomSheet: Container(
+            color: Colors.blue,
+            child: Container(
+                margin: const EdgeInsets.all(15),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text("login".tr(gender: "register_content"),
+                      style: const TextStyle(fontSize: 17),
+                      textAlign: TextAlign.center),
+                  const SizedBox(width: 10),
+                  TextButton(
+                      onPressed: _onPressRegister,
+                      style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 17),
+                          foregroundColor: Colors.yellow),
+                      child: Text(
+                        "login".tr(gender: "create_account"),
+                      ))
+                ]))),
+      )),
+    );
   }
 }
