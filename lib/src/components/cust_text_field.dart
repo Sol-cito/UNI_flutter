0824a3fec3a29e9c6f4d2bf3ec1f45a478863945
sol <dataenggu@gmail.com>
2023-04-16 18:37:53 +0200

    Make email validation work  when the state is invalid and user is typing the email field, also added test code

--- lib/src/components/cust_text_field.dart
+++ lib/src/components/cust_text_field.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustTextField extends StatefulWidget {
   final TextEditingController textEditingController;
+  final ValueChanged<String>? onChange;
   final String? hintText;
   final bool? isInputInvalid;
   final String? errorText;
@@ -15,7 +16,8 @@ class CustTextField extends StatefulWidget {
       this.isInputInvalid,
       this.errorText,
       this.isObscure,
-      this.maxLength})
+      this.maxLength,
+      this.onChange})
       : super(key: key);
 
   String? _getErrorText() {
@@ -36,20 +38,22 @@ class _CustTextFieldState extends State<CustTextField> {
   @override
   Widget build(BuildContext context) {
     return TextField(
-        cursorColor: Colors.black,
-        maxLength: widget.maxLength,
-        controller: widget.textEditingController,
-        obscureText: widget.isObscure == null ? false : widget.isObscure!,
-        decoration: InputDecoration(
-            hintText: widget.hintText ?? "",
-            errorText: widget._getErrorText(),
-            border: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black)),
-            focusedBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black)),
-            enabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black)),
-            filled: true,
-            fillColor: Colors.lightBlue));
+      cursorColor: Colors.black,
+      maxLength: widget.maxLength,
+      controller: widget.textEditingController,
+      obscureText: widget.isObscure == null ? false : widget.isObscure!,
+      decoration: InputDecoration(
+          hintText: widget.hintText ?? "",
+          errorText: widget._getErrorText(),
+          border: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black)),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black)),
+          enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black)),
+          filled: true,
+          fillColor: Colors.lightBlue),
+      onChanged: widget.onChange,
+    );
   }
 }
